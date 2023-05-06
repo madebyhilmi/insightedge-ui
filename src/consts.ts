ee5e548c752717ed1032e1222eb9290b54516cfa
Hilmi Abou-Saleh <hello@hilmi.org>
2023-05-05 19:53:47 -0700

    setup overwolf framework

---
+++ src/consts.ts
@@ -0,0 +1,37 @@
+export const kGamesFeatures = new Map<number, string[]>([
+  // League of Legends
+  [
+    5426,
+    [
+      'live_client_data',
+      'matchState',
+      'match_info',
+      'death',
+      'respawn',
+      'abilities',
+      'kill',
+      'assist',
+      'gold',
+      'minions',
+      'summoner_info',
+      'gameMode',
+      'teams',
+      'level',
+      'announcer',
+      'counters',
+      'damage',
+      'heal'
+    ]
+  ],
+]);
+
+export const kGameClassIds = Array.from(kGamesFeatures.keys());
+
+export const kWindowNames = {
+  inGame: 'in_game',
+  desktop: 'desktop'
+};
+
+export const kHotkeys = {
+  toggle: 'sample_app_ts_showhide'
+};
